a9665e7959f175661ae55a0d3b215ba89985d019
Saidafzal <said.dev1607@gmail.com>
2022-04-02 21:58:23 +0500

    conditions and operators

---
+++ main.js
@@ -0,0 +1,161 @@
+// data types
+
+
+// primitive 
+
+// number 
+
+// var age = 32 ;
+
+// console.log(age);
+
+// string 
+// var job = 'teacher';
+// job = 'fireman';
+// console.log(job)
+
+//boolean 
+
+// var isMarried = false;
+// isMarried = true;
+
+// console.log(isMarried)
+
+
+// unefined
+
+// var x  ;
+// console.log(x)
+
+
+//null;
+// var color = null ;
+// console.log(color);
+
+
+// var x = null ;
+// console.log(typeof(x), ' is value');
+
+
+
+//arithmetic operator;
+
+// var a = 4 ; 
+// var b = 7;
+// var z = b % a;
+// console.log(z)
+
+
+// var k = 2;
+// var h = k ** 8;
+// console.log(h);
+
+
+// var j = 5;
+// var j = --j;
+
+
+// var j = j +3
+
+
+// var d = 'trausers';
+// var D ;
+
+
+
+// console.log(j)
+
+
+//logical operators
+
+// var userName = 'number';
+// var userAge =  23;
+
+// var result = userName || userAge
+
+// console.log(result)
+
+
+// var x = true ; 
+// var y = 'ali' ;
+// console.log(x && y)
+
+
+//asignment
+
+// var lastName = 'Aliyev';
+// var firstName = 'Aliyev';
+
+// console.log(lastName === firstName)
+
+// var t = 9 ;  // number
+// var m = '9'; //string
+
+// console.log( t !== m) //qiymatga tekshiradi
+
+
+
+
+
+// camelcase
+
+
+// var isOverGreeting = 'good bye';
+// console.log(isOverGreeting)
+
+
+// pascal case
+
+// var SayHello = 'hi ';
+// console.log(SayHello)
+
+
+
+
+
+
+//reference
+
+//conditions 
+
+// let age = 27 ; 
+
+// if(age > 30){
+//     console.log('Assalom aleykum bratan');
+// }else{
+//     console.log('qalesan ukam');
+// }
+
+
+// let color = 'blue';
+
+// if(color === 'red'){
+//     console.log('bu kuk rang')
+// }else{
+//     console.log('bu siz tanlagan rang emas')
+// }
+
+
+// 1-masala 
+
+// o'zgaruvchi bo'ladi va uni yoshi berilgan masalan 19 ;
+
+
+// qachonki yosh 18 dan katta bo'lsa siz voyaga yetgansiz ;
+// yosh 16 dan kichik bo'lsa siz ham yoshsiz;
+// yosh 35 dan katta bo'lsa siz saylovda o'z nomzodizni qo'yishiz mumkin;
+// yosh 65 teng yoki siz hozir nafaqadasz;
+
+// 2-masala
+
+// o'zgaruvchi olib unga qiymat berasz masalan 7 ertalab
+
+
+// qachonki soat 6 10 gacha xayrli tong 
+// soat 10 18 gacha xayrli kun 
+// soat 18 dan 22 gacha xayrli kech
+
+// soat 22 dan keyin xayrli tun 
+
+
+
